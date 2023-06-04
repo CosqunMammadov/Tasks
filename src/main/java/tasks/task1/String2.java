0885ceb395202ccff6cacdcb872544be89fdbd0e
CosqunMammadov <cosqun.mammadov23@gmail.com>
2023-06-04 20:31:59 +0400

    sdf

--- src/main/java/tasks/task1/String2.java
+++ src/main/java/tasks/task1/String2.java
@@ -1,4 +1,15 @@
 package tasks.task1;
 
+import java.util.Scanner;
+
 public class String2 {
-}
+
+        public static void main(String[] args) {
+            Scanner in = new Scanner(System.in);
+            String S = in.next();
+            int start = in.nextInt();
+            int end = in.nextInt();
+            System.out.println(S.substring(start, end));
+        }
+    }
+
