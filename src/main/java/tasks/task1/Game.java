9ff55468d53abcbf6de47a381ce2c25f2a28edc4
CosqunMammadov <cosqun.mammadov23@gmail.com>
2023-06-04 19:22:32 +0400

    Initial commit

---
+++ src/main/java/tasks/task1/Game.java
@@ -0,0 +1,34 @@
+package tasks.task1;
+
+import java.util.Scanner;
+
+public class Game {
+    public static void main(String[] args) {
+        Scanner scan=new Scanner(System.in);
+        System.out.println("Çətinlik dərəcəsini yazın 10, 100 və 1000");
+        int levelOfDifficulty=scan.nextInt();
+        int randomNum = (int)(Math.random() * levelOfDifficulty);
+        // System.out.println(randomNum);
+        System.out.println("Rəqəm təxmin edin");
+        int writtenNumber=-1;
+        while (randomNum!=writtenNumber){
+            writtenNumber=scan.nextInt();
+            if (writtenNumber-randomNum>10 && writtenNumber-randomNum<100){
+                System.out.println("Çox böyükdür");
+            } else if (writtenNumber - randomNum < -10 && writtenNumber-randomNum>-100) {
+                System.out.println("Çox kiçikdir");
+            } else if (writtenNumber - randomNum > -10 && writtenNumber-randomNum<0) {
+                System.out.println("Kiçikdir");
+            } else if (writtenNumber - randomNum >10 && writtenNumber-randomNum<0) {
+                System.out.println("Böyükdür");
+            }else if (writtenNumber - randomNum < -100) {
+                System.out.println("Çox çox kiçikdir");
+            }else if (writtenNumber - randomNum >100) {
+                System.out.println("Çox çox böyükdür");
+            }else {
+                System.out.println("Təbriklər");
+                break;
+            }
+        }
+    }
+}
