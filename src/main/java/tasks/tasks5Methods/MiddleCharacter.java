8be0543d9d9853db9c2b1a93c11efc90c0689dea
CosqunMammadov <cosqun.mammadov23@gmail.com>
2023-06-20 13:00:09 +0400

    Initial commit

--- src/main/java/tasks/tasks5Methods/MiddleCharacter.java
+++ src/main/java/tasks/tasks5Methods/MiddleCharacter.java
@@ -1,25 +1,35 @@
 package tasks.tasks5Methods;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class MiddleCharacter {
     public static void main(String[] args) {
-        Scanner scan= new Scanner(System.in);
-        String userInput= scan.nextLine();
+
+
 //        findMiddleCharacter(userInput);
 //        countVowels(userInput);
         printPentagonalNumbers();
+//        findSmallestNumber();
 
     }
 
-    public static void findMiddleCharacter(String userInput){
+    public static void findMiddleCharacter(){
+
+        Scanner scan= new Scanner(System.in);
+        String userInput= scan.nextLine();
 
         if (userInput.length()%2==0)
             System.out.println(userInput.charAt(userInput.length()/2)+" and "+userInput.charAt(userInput.length()/2-1));
         else System.out.println(userInput.charAt(userInput.length()/2));
     }
 
-    public static void countVowels(String userInput){
+
+    public static void countVowels(){
+
+        Scanner scan= new Scanner(System.in);
+        String userInput= scan.nextLine();
+
         int count=0;
         for (int i = 0; i < userInput.length(); i++) {
             if (userInput.charAt(i)=='a'||userInput.charAt(i)=='e'||userInput.charAt(i)=='i'||userInput.charAt(i)=='o'||
@@ -28,6 +38,8 @@ public class MiddleCharacter {
         }
         System.out.println(count);
     }
+
+
 
     public static void printPentagonalNumbers(){
         int a1=1;
@@ -43,4 +55,21 @@ public class MiddleCharacter {
             count++;
         }
     }
+
+
+
+    public static void findSmallestNumber(){
+        Scanner scan= new Scanner(System.in);
+        System.out.println("Enter quantity of number");
+        int quantityNumber=scan.nextInt();
+
+        int[] array= new int[quantityNumber];
+        System.out.println("Enter your numbers");
+        for (int i = 0; i < quantityNumber; i++) {
+            array[i]=scan.nextInt();
+        }
+        Arrays.sort(array);
+        System.out.println(array[0]);
+    }
+
 }
