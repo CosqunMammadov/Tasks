1a0ac70745bca45a79ab07acf0847f4042ecfb72
CosqunMammadov <cosqun.mammadov23@gmail.com>
2023-07-10 13:13:21 +0400

    Initial commit

--- src/main/java/tasks/L4_Array/ArrayPrintSneak.java
+++ src/main/java/tasks/L4_Array/ArrayPrintSneak.java
@@ -4,20 +4,27 @@ import java.util.Arrays;
 
 public class ArrayPrintSneak {
     public static void main(String[] args) {
-        int[][] arr = {{1,3,5},{2,4,6},{3,5,8}};
+        int[][] arr = {{1, 3, 5},{2, 4, 6},{3, 5, 8},{9, 10, 11}};
+
+        int lenght = arr.length * arr[0].length;
+        int firstIndex = 0;
+        int x = 0;
 
 
-        for (int i = 0; i < arr.length; i++) {
-            int a = Math.round(i%2);
-            a = a*(-2);
-            int b = Math.abs(a);
-            System.out.print(arr[i][b]+", ");
-            a++;
-            b = Math.abs(a);
-            System.out.print(arr[i][b]+", ");
-            a++;
-            b = Math.abs(a);
-            System.out.print(arr[i][b]+", ");
+
+        for (int i = lenght; i > 0; i--) {
+            int residue = lenght - i;
+            int increase = (int) ((-1 * Math.signum(residue % arr[0].length)) + Math.signum(residue));
+            int a = firstIndex;
+            firstIndex = firstIndex + increase;
+
+            int factor = Math.round(firstIndex % 2);
+            x = x - ((firstIndex - a) * (2 * factor - 1));
+
+            int secondIndex = x;
+
+            System.out.print(arr[firstIndex][secondIndex] + ", ");
+            x = x + (-2 * factor) + 1;
 
         }
 
