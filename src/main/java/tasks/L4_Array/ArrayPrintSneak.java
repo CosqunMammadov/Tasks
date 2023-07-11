fc5b04d06766ad96a1d491f4eb920323365e4d35
CosqunMammadov <cosqun.mammadov23@gmail.com>
2023-07-11 20:02:29 +0400

    Initial commit

--- src/main/java/tasks/L4_Array/ArrayPrintSneak.java
+++ src/main/java/tasks/L4_Array/ArrayPrintSneak.java
@@ -4,27 +4,26 @@ import java.util.Arrays;
 
 public class ArrayPrintSneak {
     public static void main(String[] args) {
-        int[][] arr = {{1, 3, 5},{2, 4, 6},{7, 5, 8}};
+        int[][] arr = {{1, 3, 5}, {2, 4, 6}, {7, 5, 8}};
 
         int lenght = arr.length * arr[0].length;
         int firstIndex = 0;
-        int x = 0;
+        int secondIndex = -1;
 
 
 
         for (int i = lenght; i > 0; i--) {
-            int residue = lenght - i;
+            int residue = lenght - i;        //0, 1, 2, 3, 4, 5, 6, 7, 8
+
             int increase = (int) ((-1 * Math.signum(residue % arr[0].length)) + Math.signum(residue));
             int a = firstIndex;
             firstIndex = firstIndex + increase;
 
-            int factor = Math.round(firstIndex % 2);
-            x = x - ((firstIndex - a) * (2 * factor - 1));
+            int factor = (int) Math.signum(firstIndex % 2);
+            secondIndex = secondIndex + ((-2 * factor + 1) * (a - firstIndex + 1));
 
-            int secondIndex = x;
 
             System.out.print(arr[firstIndex][secondIndex] + ", ");
-            x = x + (-2 * factor) + 1;
 
         }
 
